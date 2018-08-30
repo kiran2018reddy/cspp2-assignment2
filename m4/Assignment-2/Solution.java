d227ee92369f27a9357f2a7e9c8ce30ffd257dad
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-30 15:59:29 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -17,27 +17,27 @@ public final class Solution {
 		int arr2[][] = new int[a][b];
 		int sum[][] = new int[a][b];
 		int c,d;//System.out.println("input elements in first matrix");
-		for(c = 0 ; c < a ; c = c + 1) {
+		for (c = 0 ; c < a ; c = c + 1) {
 			for (d = 0;d < b ; d = d + 1)
 				arr1[c][d] = scan.nextInt();
 		}
 	int a1 = scan.nextInt();
 		//System.out.println("input number of coloums");
 		int b1 = scan.nextInt();
-		if(a != a1 || b != b1) {
+		if (a != a1 || b != b1) {
 			System.out.println("not possible");
 		} else {
 		//System.out.println("input elements in second matrix");
-		for(c = 0 ; c < a ; c = c + 1) {
+		for (c = 0 ; c < a ; c = c + 1) {
 			for (d = 0 ;d < b ; d = d + 1)
 				arr2[c][d] = scan.nextInt();
 		}
-		for(c =0 ; c < a ; c = c + 1) {
+		for (c =0 ; c < a ; c = c + 1) {
 			for (d=0 ; d< b ; d = d + 1)
 				sum[c][d] = arr1[c][d]+arr2[c][d];
 		}
-		for(c=0; c < a; c = c + 1) {
-			for(d = 0 ; d < b ; d = d + 1) {
+		for (c=0; c < a; c = c + 1) {
+			for(d = 0; d < b; d = d + 1) {
 				System.out.print(sum[c][d]);
 				if (d < b - 1) {
 					System.out.print(" ");
