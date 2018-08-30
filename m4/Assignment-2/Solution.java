eff1ed36f266b9b88dc7ac1247de1d7a64f1e9df
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-30 15:42:42 -0700

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -25,6 +25,15 @@ public class Solution {
 	int a1 = scan.nextInt();
 		//System.out.println("input number of coloums");
 		int b1 = scan.nextInt();
+
+
+
+
+		if (a == a1 && b == b1)
+			System.out.println("possible");
+		else
+			System.out.println("not possible");
+
 		//System.out.println("input elements in second matrix");
 		for (c=0;c<a; c=c+1){
 			for (d=0;d<b;d=d+1)
@@ -32,7 +41,6 @@ public class Solution {
 		}
 			
 
-		
 		
 		for (c =0 ; c< a ;c=c+1){
 			for (d=0 ; d< b ; d=d+1)
