46f3f5979faeff46d66b559d8d34920ed66e4052
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-30 15:52:12 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 /**
  * Class for solution.
  */
@@ -10,51 +10,32 @@ public class Solution {
 	 */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		int a,b;
-		//System.out.println("input number of rows");
-		a = scan.nextInt();
-		//System.out.println("input number of coloums");
+		int a,b;//System.out.println("input number of rows");
+		a = scan.nextInt();//System.out.println("input number of coloums");
 		b = scan.nextInt();
-
 		int arr1[][]= new int[a][b];
 		int arr2[][] = new int[a][b];
 		int sum[][] = new int[a][b];
-
-
-		int c,d;
-		//System.out.println("input elements in first matrix");
+		int c,d;//System.out.println("input elements in first matrix");
 		for (c = 0 ; c < a ; c=c+1){
 			for (d=0;d < b ; d=d+1)
 				arr1[c][d] = scan.nextInt();
 		}
-	
-
 	int a1 = scan.nextInt();
 		//System.out.println("input number of coloums");
 		int b1 = scan.nextInt();
-
-
-
-
 		if (a != a1 || b != b1) {
 			System.out.println("not possible");
 		} else {
-			
-
 		//System.out.println("input elements in second matrix");
 		for (c=0;c<a; c=c+1){
 			for (d=0;d<b;d=d+1)
 				arr2[c][d] = scan.nextInt();
 		}
-			
-
-		
 		for (c =0 ; c< a ;c=c+1){
 			for (d=0 ; d< b ; d=d+1)
 				sum[c][d] = arr1[c][d]+arr2[c][d];
 		}
-
-
 		for (c=0; c < a; c = c+1){
 			for (d = 0 ; d < b ; d=d+1){
 				System.out.print(sum[c][d]);
@@ -62,9 +43,7 @@ public class Solution {
 					System.out.print(" ");
 			}
 			System.out.println();
-		}
-		
-		
+		}	
 	}
 }
 }
