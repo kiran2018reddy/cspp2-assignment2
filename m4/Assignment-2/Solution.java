09bdf34326ff4ac2c11cc0a40380d51965c89ffd
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-30 18:24:17 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -21,7 +21,7 @@ public final class Solution {
 			for (d = 0;d < b ; d = d + 1)
 				arr1[c][d] = scan.nextInt();
 		}
-	int a1 = scan.nextInt();
+	    int a1 = scan.nextInt();
 		//System.out.println("input number of coloums");
 		int b1 = scan.nextInt();
 		if (a != a1 || b != b1) {
