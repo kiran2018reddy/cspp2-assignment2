3468823fa40ad866925198b872533c072643fff0
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-30 18:31:03 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -19,10 +19,11 @@ public final class Solution {
         int arr1[][] = new int[a][b];
         int arr2[][] = new int[a][b];
         int sum[][] = new int[a][b];
-        int c,d;//System.out.println("input elements in first matrix");
+        int c, d;//System.out.println("input elements in first matrix");
         for (c = 0; c < a; c = c + 1) {
-            for (d = 0; d < b; d = d + 1)
+            for (d = 0; d < b; d = d + 1) {
                 arr1[c][d] = scan.nextInt();
+            }
         }
         int a1 = scan.nextInt();
         //System.out.println("input number of coloums");
@@ -32,12 +33,14 @@ public final class Solution {
         } else {
         //System.out.println("input elements in second matrix");
         for (c = 0; c < a; c = c + 1) {
-            for (d = 0; d < b; d = d + 1)
+            for (d = 0; d < b; d = d + 1) {
                 arr2[c][d] = scan.nextInt();
+            }
         }
         for (c = 0; c < a; c = c + 1) {
-            for (d = 0; d < b; d = d + 1)
+            for (d = 0; d < b; d = d + 1) {
                 sum[c][d] = arr1[c][d] + arr2[c][d];
+            }
         }
         for (c = 0; c < a; c = c + 1) {
             for (d = 0; d < b; d = d + 1) {
