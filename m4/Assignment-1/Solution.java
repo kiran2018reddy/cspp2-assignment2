990c3e1f32e3579e48de630d7cd21408acb1c20f
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-08-30 19:45:03 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

---
+++ m4/Assignment-1/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * { item_description }.
+ */
+import java.util.Scanner;
+/**
+ * { item_description }.
+ */
+public final class Solution {
+private Solution() {
+
+}
+ /**
+ * @param      args  The arguments
+ */
+
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int max = 0;
+        int[]a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
+
