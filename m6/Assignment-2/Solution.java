19fb40cfbad8d901b969e90986e9914d6ded68e0
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-01 11:08:54 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

---
+++ m6/Assignment-2/Solution.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+/**
+ * Write a java program to round the
+ * elements of a matrix to the nearest 100.
+ *
+ * @author : 
+ */
+final class Solution {
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //not used
+    }
+    /**
+     * Function to round the
+     * elements of a matrix to the nearest 100.
+     *
+     * @param      a     Matrix of which the elements to be rounded
+     * @param      rows     Number of rows
+     * @param      columns     Number of columns
+     *
+     * @return     Matrix of the rounded elements
+     */
+    
+ static int[][] roundHundred(final int[][] a, final int rows, final int columns) {
+        final int k=100;
+        int[][] result=new int[rows][columns];
+         for (int i =0;i<rows;i++) {
+            for (int j=0; j<columns; j++) {
+                result[i][j] = k * Math.round((float) a[i][j]/k);
+            }
+         }
+        return result;
+    }
+     /**
+     * Main function.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int m = scan.nextInt();
+        int n = scan.nextInt();
+        int[][] a = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = scan.nextInt();
+            }
+        }
+        int[][] b = roundHundred(a, m, n);
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                System.out.print(b[i][j] + " ");
+            }
+            System.out.println(b[i][n - 1]);
+        }
+    }
+}
