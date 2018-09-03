2668a79357672d00e0f3d521b4b174ce700bad3c
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-02 17:18:27 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-2/Assignment-2/Solution.java
+++ m3/Assignment-2/Assignment-2/Solution.java
@@ -3,23 +3,22 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-public class Solution {
+class Solution {
 /**
  * { function_description }.
  *
  * @param      args  The arguments
  */
-    public static void main(String[] args) {
-
-        Scanner s = new Scanner(System.in);
+    final public static void main(String[] args) {
+    	Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
         final int div = 10;
         final int adiv = 10;
         final int result = 7;
         for (int i = 1; i <= n; i++) {
-        	int j = i;
-        	while (j != 0) {
+            int j = i;
+            while (j != 0) {
         		if (j % div == result) {
         			count = count + 1;
         		}
