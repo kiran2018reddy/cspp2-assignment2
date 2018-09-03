79fa56264fdbd56ceb8ee3e3779a94181ea023fe
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-02 18:40:28 -0700

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -7,42 +7,42 @@ import java.util.Scanner;
  * Class for solution.
  */
 public class Solution{
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() { }
-	/**
-	 * { function_description }.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		int n= Integer.parseInt(sc.nextLine());
-		for(int i=0;i<=n-1;i++){
-			String s=sc.nextLine();
-			String res=binaryToDecimal(s);//Write binaryToDecimal function
-			System.out.println(res);
-		}
-	}
-	/**
-	 * { function_description }
-	 *
-	 * @param      s     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
+    /**
+     * Constructs the object.
+     */
+    private Solution() { }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for(int i = 0;i <= n - 1;i++){
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);//Write binaryToDecimal function
+            System.out.println(res);
+        }
+    }
+    /**
+     * { function_description }
+     *
+     * @param      s     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
 static String binaryToDecimal(final String s) {
-	int sum = 0;
-	int a = 1;
-	int b = s.length()-1;
-	for (int i = 0;i<s.length();i++){
-		a = (int)Math.pow(2,b);
-		sum = sum+(a*Character.getNumericValue(s.charAt(i)));
-		b = b - 1;
-	}
-	return Integer.toString(sum);
+    int sum = 0;
+    int a = 1;
+    int b = s.length() - 1;
+    for (int i = 0;i < s.length();i++){
+        a = (int)Math.pow(2,b);
+        sum = sum + (a * Character.getNumericValue(s.charAt(i)));
+        b = b - 1;
+    }
+    return Integer.toString(sum);
 
 }
 }
