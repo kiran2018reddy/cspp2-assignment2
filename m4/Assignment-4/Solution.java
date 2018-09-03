04d680d5435ea5cd4f59e2e03dabc2a629593b1b
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-02 21:04:39 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -3,19 +3,35 @@
 
 
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify the main function 
-	*/
-	public static void main(String[] args){
+/**
+ * Class for solution.
+ */
+public class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() { }
+/**
+ * { function_description }.
+ *
+ * @param      args  The arguments
+ */
+	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
 		String s=sc.nextLine();
 		String reverse=reverseString(s);	
 		System.out.println(reverse);
 	}
+	/**
+	 * { function_description }.
+	 *
+	 * @param      s     { parameter_description }.
+	 *
+	 * @return     { description_of_the_return_value }.
+	 */
 	public static String reverseString(final String s) {
 		String reverse= "";
-		for (int i =0; i< s.length(); i++){
+		for (int i = 0; i < s.length(); i++) {
 			reverse = s.charAt(i) + reverse;
 		}
 
