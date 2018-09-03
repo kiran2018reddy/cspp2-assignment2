dd209dce49c6f8b8b131348c59cf862331a2e0c9
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-02 17:29:28 -0700

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -1,19 +1,17 @@
-/**
- * { item_description }.
- */
 import java.util.Scanner;
 /**
  * { item_description }.
  */
 public final class Solution {
-/*
-private Solution() {
-
-}
- /**
+/**
+ * Constructs the object.
+ */
+    private Solution() { }
+/**
+ * { function_description }.
+ *
  * @param      args  The arguments
  */
-
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
