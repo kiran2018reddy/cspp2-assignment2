a8f872195dd6dd4d05dddf529e6d4c1d599367d6
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-02 17:19:33 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-2/Assignment-2/Solution.java
+++ m3/Assignment-2/Assignment-2/Solution.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 /**
  * Class for solution.
  */
-class Solution {
+public class Solution {
 /**
  * { function_description }.
  *
  * @param      args  The arguments
  */
     final public static void main(String[] args) {
-    	Scanner s = new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
         final int div = 10;
@@ -19,11 +19,11 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             int j = i;
             while (j != 0) {
-        		if (j % div == result) {
-        			count = count + 1;
-        		}
-        		j = Math.round(j / adiv);
-        	}
+                if (j % div == result) {
+                    count = count + 1;
+                }
+                j = Math.round(j / adiv);
+            }
         }
         System.out.println(count);
     }
