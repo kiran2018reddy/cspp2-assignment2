f957a0f56648408db6a8ec482bb43297998afafe
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-03 19:48:59 -0700

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,17 +1,44 @@
 import java.util.Scanner;
+/**
+ * Class for input validator.
+ */
 class InputValidator {
+    /**
+     * { var_description }.
+     */
     private String input;
+    /**
+     * Constructs the object.
+     *
+     * @param      giveninput  The giveninput
+     */
     InputValidator(final String giveninput) {
-        this.input =giveninput;
+        this.input = giveninput;
     }
+    /**
+     * { function_description }.
+     *
+     * @return     { description_of_the_return_value }.
+     */
     public boolean validateData() {
-        final int a=6;
-        return this.input.length()>=a;
+        final int a = 6;
+        return this.input.length() >= a;
     }
 }
+/**
+ * { item_description }.
+ */
 final class Solution {
+    /**
+     * Constructs the object.
+     */
     private Solution() {
     }
+    /**
+     * { function_description }.
+     *
+     * @param      args  The arguments
+     */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         String input = scan.next();
