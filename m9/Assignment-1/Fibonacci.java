243414c42418b4f1d3cce6b9a63c3d6478c4a0a0
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-05 10:00:23 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- m9/Assignment-1/Fibonacci.java
+++ m9/Assignment-1/Fibonacci.java
@@ -30,14 +30,13 @@ public class Fibonacci
         int febcount = n;
         int[] feb = new int[febcount];
         feb[0] = 0;
-        for (int i = 2; i<febcount;i++){
+        feb[1]= 1;
+                for (int i = 2; i<febcount;i++){
             feb[i]= feb[i-1]+ feb[i-2];
         }
         for (int i =1; i<febcount;i++){
             System.out.println(feb[i]+" ");
         }
-
-        return null;
     }
 
 	public static void main(String[] args) {
