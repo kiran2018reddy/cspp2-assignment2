088c9d52b637685d51374516fff649d95a400f3e
kiran2018reddy <kiranbolla7@msitprogram.net>
2018-09-09 19:00:56 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m10/Assignment-2/Solution.java
+++ m10/Assignment-2/Solution.java
@@ -1,16 +1,18 @@
 import java.io.BufferedInputStream;
 import java.util.Scanner;
-
+/**
+ * Class for solution.
+ */
 class Solution {
 /**
  * { function_description }.
  *
  * @param      args  The arguments.
  */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
         // create an object of the list to invoke methods on it
         StringList sl = new StringList();
-        StringListInterface l = (StringListInterface)sl; 
+        StringListInterface l = (StringListInterface) sl;
         // code to read the test cases input file
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         // check if there is one more line to process
